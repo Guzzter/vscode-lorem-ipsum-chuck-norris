45e4c5b535bdb2bf3f99d860bc1f8ce3ab75eb5a
Guus Beltman <g_beltman@yahoo.com>
2021-11-13 18:42:59 +0100

    First commit

---
+++ README.md
@@ -0,0 +1,32 @@
+A VS Code extension that generate and insert lorem ipsum text based on Chuck Norris fun facts. "Chuck Norris makes unions cry."
+
+To use the extension, open the command palette (<kbd>F1</kbd> or <kbd>cmd/ctrl+shift+p</kbd>, type "lorem chucksum" and select to insert either a single line or single/more paragraph.
+
+## Features
+
+It is very easy to generate some lines or paragraphs of text inspired by Chuck Norris:
+
+![](images/demo.gif)
+
+
+## Commands and custom keybindings
+
+The following commands are added:
+
+- `vscode-lorem-ipsum-chuck-norris.line`
+- `vscode-lorem-ipsum-chuck-norris.paragraph`
+- `vscode-lorem-ipsum-chuck-norris.multipleParagraphs`
+
+By default they don't have keybindings, but [you can add a custom keybinding](https://code.visualstudio.com/docs/getstarted/keybindings#_advanced-customization). For example:
+
+```json
+{
+  "key": "ctrl+shift+l",
+  "command": "vscode-lorem-ipsum-chuck-norris.multipleParagraphs",
+  "when": "editorTextFocus"
+}
+
+## Credits
+
+- Tyler Leonhardt for demonstrating [building a VS extension](https://www.youtube.com/watch?v=PGAu06_E_BU)
+- Daniel Imms providing an example [lorem-ipsum extension](https://github.com/Tyriar/vscode-lorem-ipsum)
---
+++ src/extension.ts
@@ -0,0 +1,74 @@
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+import * as vscode from 'vscode';
+import {getLoremChucksum} from 'lorem-chucksum/lib/index.js';
+
+async function insertText(loremWanted: { count: number, units: string }) {
+	let editor = vscode.window.activeTextEditor;4
+	if (editor !== undefined) {
+		editor.edit(edit => editor.selections.forEach(
+			selection => {
+				edit.delete(selection);
+				edit.insert(selection.start, getLoremChucksum(loremWanted));
+			}
+		));
+	}
+}
+
+async function generateLine() {
+	insertText({
+		count: 1,
+		units: 'sentences'
+	});
+
+	showThanksMessage();
+}
+
+async function generateParagraph() {
+	insertText({
+	  count: 1,
+	  units: 'paragraphs'
+	});
+
+	showThanksMessage();
+}
+
+async function generateMultipleParagraphs() {
+	const paragraphOptions = [];
+	for (let i = 2; i <= 10; i++) {
+		paragraphOptions.push(i.toString() + ' paragraphs');
+	}
+
+	const count = await vscode.window.showQuickPick(paragraphOptions, { placeHolder: 'How many paragraphs?' });
+	if (!count) {
+	  return;
+	}
+
+	insertText({
+	  count: Number.parseInt(count),
+	  units: 'paragraphs'
+	});
+
+	showThanksMessage();
+}
+
+function showThanksMessage() {
+	vscode.window.setStatusBarMessage('Chuck Norris has you covered!');
+}
+
+// this method is called when your extension is activated
+// your extension is activated the very first time the command is executed
+export async function activate(context: vscode.ExtensionContext) {
+	let commands = [
+		vscode.commands.registerCommand('vscode-lorem-ipsum-chuck-norris.line', generateLine),
+		vscode.commands.registerCommand('vscode-lorem-ipsum-chuck-norris.paragraph', generateParagraph),
+		vscode.commands.registerCommand('vscode-lorem-ipsum-chuck-norris.multipleParagraphs', generateMultipleParagraphs)
+	];
+
+	commands.forEach(function(command) {
+		context.subscriptions.push(command);
+	});
+}
+
+// this method is called when your extension is deactivated
+export function deactivate() {}
